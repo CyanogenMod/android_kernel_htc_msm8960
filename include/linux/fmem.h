02870a02c4c48c737b76d406fbc108e9031e35eb
Olav Haugan <ohaugan@codeaurora.org>
2012-02-15 09:11:55 -0800

    gpu: ion: Move MFC heap to different address
    Video hardware has the following requirements for
    ION heaps:
    
    1. MM heap must be at a higher address than FW heap.
    2. MFC heap must at a higher address than FW
    3. MM heap must be adjacent to FW heap.
       (There cannot be another heap between FW and MM heap)
    4. MM and MFC heap cannot be more than 256MB away
       from the base address of the FW heap.
    
    MM heap is configured as a reusable heap (FMEM heap) and FMEM
    is carved out at a much higher address than the other heaps breaking
    the above requirements. To support the above requirements the
    MFC heap together with the FW heap must be carved out at the same
    location as MM heap.
    
    Change-Id: Ie0acb4b267d4307190ea3cd9ff23c710ffa1a538
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- include/linux/fmem.h
+++ include/linux/fmem.h
@@ -20,7 +20,8 @@
 struct fmem_platform_data {
 	unsigned long phys;
 	unsigned long size;
-	unsigned long reserved_size;
+	unsigned long reserved_size_low;
+	unsigned long reserved_size_high;
 	unsigned long align;
 };
 
@@ -29,7 +30,8 @@ struct fmem_data {
 	void *virt;
 	struct vm_struct *area;
 	unsigned long size;
-	unsigned long reserved_size;
+	unsigned long reserved_size_low;
+	unsigned long reserved_size_high;
 };
 
 enum fmem_state {
