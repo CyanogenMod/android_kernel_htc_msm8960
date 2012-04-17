1d3e181a050cf1218c8c4c7ae7bb9ec7254ef6b0
Laura Abbott <lauraa@codeaurora.org>
2012-04-17 11:14:30 -0700

    msm: Make iommu domains platform devices
    Information about the layout of iommu domains is a device
    specific option. Convert the iommu domain information
    into a platform driver with a corresponding platform device.
    This allows different devices to have different layouts as
    needed.
    
    Change-Id: I6bf4162476143daabe16a2f5de3b655ae4377902
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	arch/arm/mach-msm/board-8930.c
    	arch/arm/mach-msm/board-8960.c
    	arch/arm/mach-msm/devices-8064.c
    	arch/arm/mach-msm/devices-8960.c
    	arch/arm/mach-msm/devices.h

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -254,3 +254,7 @@ extern struct platform_device msm_tpiu_device;
 extern struct platform_device msm_funnel_device;
 extern struct platform_device msm_etm_device;
 #endif
+
+extern struct platform_device msm8960_iommu_domain_device;
+extern struct platform_device msm8930_iommu_domain_device;
+extern struct platform_device apq8064_iommu_domain_device;
