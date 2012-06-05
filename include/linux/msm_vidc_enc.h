d040f0713af7ac9ce74a7ca37ccaf02488ce0f37
Ethan Chen <intervigil@gmail.com>
2012-06-05 12:19:21 -0700

    include: linux: msm_vidc_enc: revert HTC's weird pbuffer definition

--- include/linux/msm_vidc_enc.h
+++ include/linux/msm_vidc_enc.h
@@ -598,12 +598,10 @@ struct venc_msg{
 };
 
 struct venc_recon_addr{
+	unsigned char *pbuffer;
 	unsigned long buffer_size;
 	unsigned long pmem_fd;
 	unsigned long offset;
-/* HTC START */
-	unsigned char *pbuffer;
-/* HTC END */
 };
 
 struct venc_recon_buff_size{
