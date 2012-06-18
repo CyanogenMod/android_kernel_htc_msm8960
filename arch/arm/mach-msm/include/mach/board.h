6b502e1e670258327ff79e56b587bc700eabd9b8
Huaibin Yang <huaibiny@codeaurora.org>
2012-06-18 15:17:41 -0700

    msm: display: remove the predefined mdp clk rate tables
    Mdp clk rate will be calculated during run time based on pipe
    requirements, so the tables in board files are not necessary. Only the
    max rate which is target specific is passed to display driver.
    
    Change-Id: Ib42d378e5bb4b6876719c2cebefbd9dc9a4cef15
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -386,11 +386,9 @@ struct msm_panel_common_pdata {
 	void (*panel_config_gpio)(int);
 	int (*vga_switch)(int select_vga);
 	int *gpio_num;
-	int mdp_core_clk_rate;
-	unsigned num_mdp_clk;
-	int *mdp_core_clk_table;
 	int (*rgb_format)(void);
 	unsigned char (*shrink_pwm)(int val);
+	u32 mdp_max_clk;
 #ifdef CONFIG_MSM_BUS_SCALING
 	struct msm_bus_scale_pdata *mdp_bus_scale_table;
 #endif
