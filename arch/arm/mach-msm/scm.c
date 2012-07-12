af27abb1ecb18e6f9bd0085f93b1acc7c809992c
Steve Muckle <smuckle@codeaurora.org>
2012-07-12 15:51:14 -0700

    various: compilation fixes for gcc 4.6
    These changes are required to compile successfully with
    gcc version 4.6.
    
    The -Wno-address option is applied to some files to
    bypass a warning triggered by an rcu macro. The
    warning is fixed cleanly upstream, but that fix is too
    risky to backport, so it is better to just silence it
    this way.
    
    Change-Id: I787ad2db6e649d29874b7b624c2b28f686236466
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-8930.c
    	arch/arm/mach-msm/scm.c

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -213,6 +213,9 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
+#ifdef REQUIRES_SEC
+			".arch_extension sec\n"
+#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0)
 			: "r" (r0), "r" (r1), "r" (r2)
@@ -335,6 +338,9 @@ s32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
+#ifdef REQUIRES_SEC
+			".arch_extension sec\n"
+#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -367,6 +373,9 @@ s32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
+#ifdef REQUIRES_SEC
+			".arch_extension sec\n"
+#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
@@ -394,6 +403,9 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
+#ifdef REQUIRES_SEC
+			".arch_extension sec\n"
+#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
