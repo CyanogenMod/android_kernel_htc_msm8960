d51aa08f576afe4cd9348f460a83aea61d278a99
Jongrak Kwon <jongrak.kwon@lge.com>
2012-09-19 00:55:03 -0700

    msm: dsi: use timeout for dma transfer
    Use wait_for_completion_timeout instead of wait_for_completion
    not to stuck in the kernel even when the DMA fails.
    
    Change-Id: I29702c3c62f78399c173d660bbbc17a07ed27468
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    Conflicts:
    	drivers/video/msm/mipi_dsi_host.c

--- drivers/video/msm/mipi_dsi_host.c
+++ drivers/video/msm/mipi_dsi_host.c
@@ -1040,7 +1040,10 @@ void mipi_dsi_mdp_busy_wait(struct msm_fb_data_type *mfd)
 		/* wait until DMA finishes the current job */
 		pr_debug("%s: pending pid=%d\n",
 				__func__, current->pid);
-		wait_for_completion(&dsi_mdp_comp);
+		if (!wait_for_completion_timeout(&dsi_mdp_comp,
+					msecs_to_jiffies(200))) {
+			pr_err("%s: dma timeout error\n", __func__);
+		}
 	}
 	pr_debug("%s: done pid=%d\n",
 			__func__, current->pid);
@@ -1442,7 +1445,6 @@ int mipi_dsi_cmds_rx_new(struct dsi_buf *tp, struct dsi_buf *rp,
 
 int mipi_dsi_cmd_dma_tx(struct dsi_buf *tp)
 {
-
 	unsigned long flags;
 
 #ifdef DSI_HOST_DEBUG
