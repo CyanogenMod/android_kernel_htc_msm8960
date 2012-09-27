8d95083322fba050190be234749e0e8f3d9036f6
Devin Kim <dojip.kim@lge.com>
2012-09-26 17:53:12 -0700

    msm_fb: call the backlight control before FB_BLANK_POWERDOWN
    You might see the white line while lcd off. You should turn lcd backlight
    off before lcd off to avoid that issue. So call the notifier for backlight
    before handling of FB_BLANK_POWERDOWN in msm_fb.
    
    Change-Id: I3bd08025a949ec02a1fba7cef98a13d8977c135b

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1040,6 +1040,12 @@ static int msm_fb_blank(int blank_mode, struct fb_info *info)
 		return 0;
 #endif
 
+	if (blank_mode == FB_BLANK_POWERDOWN) {
+		struct fb_event event;
+		event.info = info;
+		event.data = &blank_mode;
+		fb_notifier_call_chain(FB_EVENT_BLANK, &event);
+	}
 	msm_fb_pan_idle(mfd);
 	if (mfd->op_enable == 0) {
 		if (blank_mode == FB_BLANK_UNBLANK)
