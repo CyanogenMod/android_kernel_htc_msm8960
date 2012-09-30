98512215c8ae9ea35b700ccd101888b54ff1c145
Ethan Chen <intervigil@gmail.com>
2012-09-29 19:45:35 -0700

    msm: add extra cpu detection function stubs
    Change-Id: I4b4c78a84ce04172a9dd305e1a3b45fdfd94feb2

--- arch/arm/mach-msm/include/mach/socinfo.h
+++ arch/arm/mach-msm/include/mach/socinfo.h
@@ -233,6 +233,11 @@ static inline int cpu_is_msm8930(void)
 #endif
 }
 
+static inline int cpu_is_msm8930aa(void)
+{
+	return 0;
+}
+
 static inline int cpu_is_msm8627(void)
 {
 /* 8930 and 8627 will share the same CONFIG_ARCH type unless otherwise needed */
@@ -266,4 +271,10 @@ static inline int cpu_is_msm9615(void)
 	return 0;
 #endif
 }
+
+static inline int cpu_is_msm8625(void)
+{
+	return 0;
+}
+
 #endif
