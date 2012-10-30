0fcea648a0f53cdf563f1434565f7cd92aebc631
Taniya Das <tdas@codeaurora.org>
2012-10-30 15:28:20 +0530

    ARM: Fix negative idle stats for offline cpu
    We see negative idle stats because of cpu dying without
    cleaning up idle entry statistics. When a cpu is offline,
    the most immediate thing you'd want to do is just call
    into cpu_die() and not waste time calling notifier(IDLE_START).
    
    CRs-Fixed: 414554
    Change-Id: Iadc6a3ca39997e0ccf65d2a29b004e24b1b211a1
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -229,11 +229,6 @@ void cpu_idle(void)
 		tick_nohz_stop_sched_tick(1);
 		idle_notifier_call_chain(IDLE_START);
 		while (!need_resched()) {
-#ifdef CONFIG_HOTPLUG_CPU
-			if (cpu_is_offline(smp_processor_id()))
-				cpu_die();
-#endif
-
 			local_irq_disable();
 #ifdef CONFIG_PL310_ERRATA_769419
 			wmb();
@@ -259,6 +254,10 @@ void cpu_idle(void)
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
+#ifdef CONFIG_HOTPLUG_CPU
+		if (cpu_is_offline(smp_processor_id()))
+			cpu_die();
+#endif
 	}
 }
 
