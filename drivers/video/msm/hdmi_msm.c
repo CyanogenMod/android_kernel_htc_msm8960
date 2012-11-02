5e74db80e0523f3fbb8977ce88f7a7741e20b9f4
Ajay Singh Parmar <aparmar@codeaurora.org>
2012-11-03 02:39:20 +0530

    msm_fb: hdmi: black out pipes on unset
    For HDMI as primary case, we need to blackout the pipes
    when they are unset so that any residue is not visible
    on the DTV during suspend.
    
    Change-Id: I2bf36f1002dd185c26fe6c4291977737daa73ae1
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/msm_fb.h

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -2981,8 +2981,6 @@ static void hdmi_msm_hdcp_enable(void)
 	hdmi_msm_state->hdcp_activating = TRUE;
 	mutex_unlock(&hdmi_msm_state_mutex);
 
-	fill_black_screen();
-
 	mutex_lock(&hdcp_auth_state_mutex);
 	/*
 	 * Initialize this to zero here to make
@@ -3023,8 +3021,6 @@ static void hdmi_msm_hdcp_enable(void)
 	ret = hdcp_authentication_part3(found_repeater);
 	if (ret)
 		goto error;
-
-	unfill_black_screen();
 
 	mutex_lock(&hdmi_msm_state_mutex);
 	hdmi_msm_state->hdcp_activating = FALSE;
