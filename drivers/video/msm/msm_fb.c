6a581670e7a53783d6f4063da36737cfd65eb4b8
Steve Kondik <shade@chemlab.org>
2012-12-30 00:00:06 -0800

    video: msm: Initialize screeninfo in pan_display
     * Commit aff691a66c0a1371307dbbe4daf8143a7f643b8a ("non-blocking for
       overlay update") refactored a couple of structs and moved the
       screeninfo to a different place. Previously, pan_display passed the
       screeninfo to the internal function as an argument, but this has
       changed. Initialize the struct in the new location correctly.
     * This fixes the "framebuffer skips every other frame" issue seen when
       MDP composition is not in use (recovery, etc).
    
    Change-Id: I68cd201ed6a093362561a151be5a7a81f357980f

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1866,6 +1866,7 @@ static int msm_fb_pan_display(struct fb_var_screeninfo *var,
 	struct mdp_display_commit disp_commit;
 	memset(&disp_commit, 0, sizeof(disp_commit));
 	disp_commit.wait_for_finish = TRUE;
+	memcpy(&disp_commit.var, var, sizeof(struct fb_var_screeninfo));
 	return msm_fb_pan_display_ex(info, &disp_commit);
 }
 
