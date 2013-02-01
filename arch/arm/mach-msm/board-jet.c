f9e428050a67eb7a65df9997e287537c44a5ff81
Ethan Chen <intervigil@gmail.com>
2013-02-01 00:52:26 -0800

    msm: HTC 8960: increase ION SF heap size
    Change-Id: I9e28e68df31bbbd02871d8b4df8abf185ff0284c

--- arch/arm/mach-msm/board-jet.c
+++ arch/arm/mach-msm/board-jet.c
@@ -317,7 +317,7 @@ void jet_lcd_id_power(int pull)
 
 #define MSM_PMEM_ADSP_SIZE         0x65D0000 /* Need to be multiple of 64K */
 #define MSM_PMEM_AUDIO_SIZE        0x4CF000
-#define MSM_PMEM_SIZE 0x4000000 /* 64 Mbytes */
+#define MSM_PMEM_SIZE 0x5000000 /* 80 Mbytes */
 #define MSM_LIQUID_PMEM_SIZE 0x4000000 /* 64 Mbytes */
 
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
