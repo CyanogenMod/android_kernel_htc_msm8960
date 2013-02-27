70369eb576389c6bb5920f2d77e07e98f134bb48
jared waters <kushdeck@gmail.com>
2013-02-26 19:14:50 -0800

    Revert "jewel: adjust refx100 to 6650."
    This reverts commit d6cf6ae45a03d34d1c5b5d98d791e4b84001378f.

--- arch/arm/mach-msm/display/board-jet-panel.c
+++ arch/arm/mach-msm/display/board-jet-panel.c
@@ -3224,7 +3224,7 @@ static int mipi_video_auo_hd720p_init(void)
 #endif
 	pinfo.lcd.vsync_enable = TRUE;
 	pinfo.lcd.hw_vsync_mode = TRUE;
-	pinfo.lcd.refx100 = 6650; /* adjust refx100 to prevent tearing */
+	pinfo.lcd.refx100 = 6096; /* adjust refx100 to prevent tearing */
 	pinfo.mipi.te_sel = 1; /* TE from vsycn gpio */
 	pinfo.mipi.interleave_max = 1;
 	pinfo.mipi.insert_dcs_cmd = TRUE;
@@ -3366,7 +3366,7 @@ static int mipi_video_sony_hd720p_init(void)
 #endif
 	pinfo.lcd.vsync_enable = TRUE;
 	pinfo.lcd.hw_vsync_mode = TRUE;
-	pinfo.lcd.refx100 = 6650; /* adjust refx100 to prevent tearing */
+	pinfo.lcd.refx100 = 6096; /* adjust refx100 to prevent tearing */
 	pinfo.mipi.te_sel = 1; /* TE from vsycn gpio */
 	pinfo.mipi.interleave_max = 1;
 	pinfo.mipi.insert_dcs_cmd = TRUE;
