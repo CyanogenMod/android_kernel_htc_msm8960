b86581e7650ee443041b3b1c211267a2f378d6f4
Flemmard <flemmard@gmail.com>
2013-06-12 14:18:40 -0700

    msm: HTC: add missing cam_clk
    Used by s5k6a1gx front facing camera.
    
    Change-Id: I64ff71d33653385576631431213412ac69d6488a

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -5773,6 +5773,7 @@ static struct clk_lookup msm_clocks_8960_common[] __initdata = {
 	CLK_LOOKUP("cam_clk",		cam2_clk.c,		NULL),
 	CLK_LOOKUP("cam_clk",		cam0_clk.c,	"4-0020"),
 	CLK_LOOKUP("cam_clk",		cam0_clk.c,	"4-0034"),
+	CLK_LOOKUP("cam_clk",		cam1_clk.c,	"4-0036"),
 	CLK_LOOKUP("cam_clk",		cam1_clk.c,	"4-003c"),
 	CLK_LOOKUP("csi_src_clk",	csi0_src_clk.c,		"msm_csid.0"),
 	CLK_LOOKUP("csi_src_clk",	csi1_src_clk.c,		"msm_csid.1"),
