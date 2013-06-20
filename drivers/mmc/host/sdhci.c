10e5b0361455ab787d3b5418f6ac19c259ea1568
Ethan Chen <intervigil@gmail.com>
2013-06-20 11:34:12 -0700

    misc: Fix merge conflicts from 3.4.10 stable merge
    * MSM8X60 should select ARM_ERRATA_720789
    * SDHCI driver does not allow for tuning opcode
    * Remove unused variable in ext4/resize.c
    
    Change-Id: I2acf14e162f5b4dfa27a9bf088a2db218aeb5a99

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1251,7 +1251,7 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		if ((host->flags & SDHCI_NEEDS_RETUNING) &&
 		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ))) {
 			spin_unlock_irqrestore(&host->lock, flags);
-			sdhci_execute_tuning(mmc, mrq->cmd->opcode);
+			sdhci_execute_tuning(mmc);
 			spin_lock_irqsave(&host->lock, flags);
 
 			/* Restore original mmc_request structure */
