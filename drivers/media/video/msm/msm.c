b3fa3c25f7ef0047729f2088f8044a555e71437e
Ethan Chen <intervigil@gmail.com>
2013-10-21 02:03:03 -0700

    msm: camera: fix external variable typing
    Change-Id: I32b365b2a376f7b8e6df63ba8a5a1d900559377f

--- drivers/media/video/msm/msm.c
+++ drivers/media/video/msm/msm.c
@@ -4138,7 +4138,7 @@ error:
 	kobject_del(camera_attrs_obj);
 	return ret;
 }
-extern int system_rev;
+extern unsigned int system_rev;
 
 static int __devinit msm_camera_probe(struct platform_device *pdev)
 {
