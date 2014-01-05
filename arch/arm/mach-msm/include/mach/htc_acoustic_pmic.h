9dc34f38fdbd841a6d5f1b0ee0a34e2c6b6581a8
Flemmard <flemmard@gmail.com>
2014-01-05 00:13:45 -0800

    misc: rt5501: Add HTC RT5501 Amp driver
    * HTC kernel version: m7-kk-3.4.10-17db3b4
    
    Change-Id: I0b067aebecd6c36aa3bdeb32fad2b30aadc11f49

---
+++ arch/arm/mach-msm/include/mach/htc_acoustic_pmic.h
@@ -0,0 +1,22 @@
+/* include/asm/mach-msm/htc_acoustic_pmic.h
+ *
+ * Copyright (C) 2012 HTC Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef _ARCH_ARM_MACH_MSM_HTC_ACOUSTIC_PMIC_H_
+#define _ARCH_ARM_MACH_MSM_HTC_ACOUSTIC_PMIC_H_
+
+int pm8921_aud_set_s4_auto(void);
+int pm8921_aud_set_s4_pfm(void);
+int pm8921_aud_set_s4_pwm(void);
+#endif
+
