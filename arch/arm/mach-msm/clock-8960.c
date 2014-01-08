1828df1181bd35110738bbf60adce0ed54ddb210
Flemmard <flemmard@gmail.com>
2014-01-07 20:54:04 -0800

    msm: HTC: Add M7 AIF clock entry
    Change-Id: I9d585f197f77a9c2da8a4dc7a44d3b9fd0e80836

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -4537,6 +4537,9 @@ static struct clk_freq_tbl clk_tbl_aif_osr_393[] = {
 	F_AIF_OSR( 8192000, pll4, 4, 1,  12),
 	F_AIF_OSR(12288000, pll4, 4, 1,   8),
 	F_AIF_OSR(24576000, pll4, 4, 1,   4),
+#ifdef CONFIG_MACH_M7_UL
+	F_AIF_OSR(18432000, pll4, 4, 3,  16),
+#endif
 	F_AIF_OSR(27000000, pxo,  1, 0,   0),
 	F_END
 };
