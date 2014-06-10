c250b35bf6388671f3e80d710c3e3695ee71c10a
Ethan Chen <intervigil@gmail.com>
2014-06-10 00:25:51 -0700

    msm: HTC: elite: Use CMD_CLK_CTRL for backlight enable
    Change-Id: Ia1e03eb57254a673b1de1e1b632c8e43bd7f36be

--- arch/arm/mach-msm/htc/elite/display/mipi_elite.c
+++ arch/arm/mach-msm/htc/elite/display/mipi_elite.c
@@ -9,7 +9,6 @@
 
 static struct msm_panel_common_pdata *mipi_elite_pdata;
 static int mipi_elite_lcd_init(void);
-static struct dcs_cmd_req cmdreq;
 // Selected codes
 static struct dsi_cmd_desc *elite_video_on_cmds = NULL;
 int elite_video_on_cmds_count = 0;
@@ -1424,7 +1423,8 @@ static struct dsi_cmd_desc *cabc_cmds = cabc_off; /* default disable cabc */
 
 #endif
 
-static int elite_send_display_cmds(struct dsi_cmd_desc *cmd, int cnt)
+static int elite_send_display_cmds(struct dsi_cmd_desc *cmd, int cnt,
+		bool clk_ctrl)
 {
 	int ret = 0;
 	struct dcs_cmd_req cmdreq;
@@ -1432,6 +1432,8 @@ static int elite_send_display_cmds(struct dsi_cmd_desc *cmd, int cnt)
 	cmdreq.cmds = cmd;
 	cmdreq.cmds_cnt = cnt;
 	cmdreq.flags = CMD_REQ_COMMIT;
+	if (clk_ctrl)
+		cmdreq.flags |= CMD_CLK_CTRL;
 	cmdreq.rlen = 0;
 	cmdreq.cb = NULL;
 
@@ -1465,9 +1467,11 @@ static int mipi_elite_lcd_on(struct platform_device *pdev)
 	}
 
 	if (panel_type != PANEL_ID_ELITE_SHARP_HX) {
-		elite_send_display_cmds(nvt_LowTemp_wrkr_enter, ARRAY_SIZE(nvt_LowTemp_wrkr_enter));
+		elite_send_display_cmds(nvt_LowTemp_wrkr_enter,
+				ARRAY_SIZE(nvt_LowTemp_wrkr_enter), false);
 
-		elite_send_display_cmds(nvt_LowTemp_wrkr_exit, ARRAY_SIZE(nvt_LowTemp_wrkr_exit));
+		elite_send_display_cmds(nvt_LowTemp_wrkr_exit,
+				ARRAY_SIZE(nvt_LowTemp_wrkr_exit), false);
 
 		gpio_set_value(ELITE_GPIO_LCD_RSTz, 0);
 		hr_msleep(1);
@@ -1482,7 +1486,8 @@ static int mipi_elite_lcd_on(struct platform_device *pdev)
 			panel_type == PANEL_ID_ELITE_SHARP_NT_C1 ||
 			panel_type == PANEL_ID_ELITE_SHARP_NT_C2 ||
 			panel_type == PANEL_ID_ELITE_SHARP_HX) {
-		elite_send_display_cmds(elite_video_on_cmds, elite_video_on_cmds_count);
+		elite_send_display_cmds(elite_video_on_cmds,
+				elite_video_on_cmds_count, false);
 		pr_info("%s: panel_type (%d)", __func__, panel_type);
 	} else
 		pr_err("%s: panel_type is not supported!(%d)", __func__, panel_type);
@@ -1506,7 +1511,8 @@ static int mipi_elite_lcd_off(struct platform_device *pdev)
 		return 0;
 
 	if (panel_type != PANEL_ID_NONE)
-		elite_send_display_cmds(elite_display_off_cmds, elite_display_off_cmds_count);
+		elite_send_display_cmds(elite_display_off_cmds,
+				elite_display_off_cmds_count, false);
 
 	mipi_lcd_on = 0;
 
@@ -1543,22 +1549,12 @@ static void mipi_elite_set_backlight(struct msm_fb_data_type *mfd)
 	}
 
 	if (mfd->bl_level == 0) {
-		cmdreq.cmds = disable_dim;
-		cmdreq.cmds_cnt = ARRAY_SIZE(disable_dim);
-		cmdreq.flags = CMD_REQ_COMMIT;
-		cmdreq.rlen = 0;
-		cmdreq.cb = NULL;
-
-		mipi_dsi_cmdlist_put(&cmdreq);
+		elite_send_display_cmds(disable_dim, ARRAY_SIZE(disable_dim),
+				false);
 	}
 
-	cmdreq.cmds = elite_cmd_backlight_cmds;
-	cmdreq.cmds_cnt = elite_cmd_backlight_cmds_count;
-	cmdreq.flags = CMD_REQ_COMMIT;
-	cmdreq.rlen = 0;
-	cmdreq.cb = NULL;
-
-	mipi_dsi_cmdlist_put(&cmdreq);
+	elite_send_display_cmds(elite_cmd_backlight_cmds,
+			elite_cmd_backlight_cmds_count, true);
 }
 
 static void mipi_elite_per_panel_fcts_init(void)
@@ -1620,10 +1616,11 @@ static int __devinit mipi_elite_lcd_probe(struct platform_device *pdev)
 
 /* HTC specific functionality */
 #ifdef CONFIG_FB_MSM_CABC
-void mipi_elite_enable_ic_cabc(int cabc, bool dim_on, struct msm_fb_data_type *mfd)
+void mipi_elite_enable_ic_cabc(int cabc, bool dim_on,
+		struct msm_fb_data_type *mfd)
 {
 	if (dim_on)
-		dim_cmds = enable_dim;
+		cabc_cmds = enable_dim;
 	if (cabc == 1)
 		cabc_cmds = cabc_on_ui;
 	else if (cabc == 2)
@@ -1631,13 +1628,7 @@ void mipi_elite_enable_ic_cabc(int cabc, bool dim_on, struct msm_fb_data_type *m
 	else if (cabc == 3)
 		cabc_cmds = cabc_on_moving;
 
-	cmdreq.cmds = dim_cmds;
-	cmdreq.cmds_cnt = ARRAY_SIZE(enable_dim);
-	cmdreq.flags = CMD_REQ_COMMIT;
-	cmdreq.rlen = 0;
-	cmdreq.cb = NULL;
-
-	mipi_dsi_cmdlist_put(&cmdreq);
+	elite_send_display_cmds(cabc_cmds, ARRAY_SIZE(cabc_cmds), false);
 }
 #endif
 
