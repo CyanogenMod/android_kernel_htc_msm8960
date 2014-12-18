53b99cfb35ea900b188ed7136f35fa475f535e0c
Brinly Taylor <uberlaggydarwin@gmail.com>
2014-12-18 16:21:31 +1030

    power: pm8921-charger: Add HTC zara support
    Change-Id: I78b542d625be522253503c3e15e621ac50f468be

--- drivers/power/pm8921-charger-htc.c
+++ drivers/power/pm8921-charger-htc.c
@@ -6099,7 +6099,7 @@ static int __devinit pm8921_chg_hw_init(struct pm8921_chg_chip *chip)
 	u8 subrev;
 
 	if (pm8xxx_get_version(chip->dev->parent) == PM8XXX_VERSION_8921) {
-#if defined(CONFIG_MACH_M7_UL) || defined(CONFIG_MACH_M4_UL) || defined(CONFIG_MACH_T6_UL)
+#if defined(CONFIG_MACH_M7_UL) || defined(CONFIG_MACH_M4_UL) || defined(CONFIG_MACH_T6_UL) || defined(CONFIG_MACH_ZARA)
 		chip->lockup_lpm_wrkarnd = true;
 #else
 		chip->lockup_lpm_wrkarnd = false;
